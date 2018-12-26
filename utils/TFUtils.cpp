4cdff474fe945380f38d0b02047ff70653985558
skylook <skylook@126.com>
2018-12-26 18:12:14 +0800

    Update copyright headers

--- utils/TFUtils.cpp
+++ utils/TFUtils.cpp
@@ -1,5 +1,5 @@
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-// Copyright (c) 2018 Daniil Goncharov <neargye@gmail.com>.
+// Copyright (c) 2018 Liu Xiao <liuxiao@foxmail.com> and Daniil Goncharov <neargye@gmail.com>.
 //
 // Permission is hereby  granted, free of charge, to any  person obtaining a copy
 // of this software and associated  documentation files (the "Software"), to deal
